836cfe21f8c7109dd74370af75c87d24b4d56a13
abhishek goyal <goyalabhi2005@gmail.com>
2025-03-17 15:31:08 +0530

    Added Navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import UploadPage from "./pages/UploadPage";
 import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import Navbar from "./components/Navbar"; // Import Navbar
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Navbar /> {/* Add Navbar here */}
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/upload" element={<UploadPage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
---
+++ frontend/src/pages/UploadPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UploadPage = () => {
+  return (
+    <div>
+      <h1>Upload Page</h1>
+      <p>Upload your resume here.</p>
+    </div>
+  );
+};
+
+export default UploadPage;
